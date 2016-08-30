ac78cd6e9d2ad890d34b155249fa9975b1429d54
Kevin Ong <kovalian@gmail.com>
2016-08-30 16:47:22 +0800

    Prevent fractional CWND from being overridden
    Prevent Nice from erroneously resetting CWND to '2' at the end of each RTT if fractional CWND is in use.

--- tcp_nice.c
+++ tcp_nice.c
@@ -307,7 +307,7 @@ static void tcp_nice_cong_avoid(struct sock *sk, u32 ack, u32 acked)
 				}
 			}
 
-			if (tp->snd_cwnd < 2)
+			if (tp->snd_cwnd < 2 && nice->fractional_cwnd == 2)
 				tp->snd_cwnd = 2;
 			else if (tp->snd_cwnd > tp->snd_cwnd_clamp)
 				tp->snd_cwnd = tp->snd_cwnd_clamp;
